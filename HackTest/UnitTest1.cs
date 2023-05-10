46a2113673dbd66d7d78914116c2cc374b483644
Morten Bock <mbs@vertica.dk>
2023-05-10 16:01:33 +0200

    First tests

--- HackTest/UnitTest1.cs
+++ HackTest/UnitTest1.cs
@@ -1,9 +1,34 @@
+using HackApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace HackTest;
 
-public class UnitTest1
+public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public void Test1()
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public UnitTest1(WebApplicationFactory<Program> factory)
     {
+        _factory = factory;
+    }
+    [Fact]
+    public async Task Test1()
+    {
+        var client = _factory.CreateClient();
+
+        var forecasts = await client.GetFromJsonAsync<WeatherForecast[]>("/WeatherForecast");
+
+        Assert.Equal(5, forecasts?.Length);
+        
+    }
+    [Fact]
+    public async Task Test2()
+    {
+        var client = _factory.CreateClient();
+
+        var forecasts = await client.GetFromJsonAsync<WeatherForecast>("/WeatherForecast/One");
+
+        Assert.Equal(5, forecasts.TemperatureC);
+        
     }
 }
