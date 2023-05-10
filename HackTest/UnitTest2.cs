2e8ff12a458d994a709076eb291ec18facad3a22
James Sejer Canham <jsc@vertica.dk>
2023-05-10 17:30:32 +0200

    wip

--- HackTest/UnitTest2.cs
+++ HackTest/UnitTest2.cs
@@ -17,18 +17,18 @@ public class UnitTest2 : IClassFixture<WebApplicationFactory<Program>>
     [Fact]
     public async Task The_user_can_get_a_weather_forecast1()
     {
-        var forecast = await _systemUnderTest.GetFromJsonAsync<WeatherForecastDto>("/WeatherForecast/One");
+        var forecast = await _systemUnderTest.GetFromJsonAsync<WeatherForecastViewModel>("/WeatherForecast/One");
 
         Assert.Equal("2000-01-01T00:00:00", forecast.Date);
         Assert.Equal(10, forecast.TemperatureC);
         Assert.Equal(49, forecast.TemperatureF);
         Assert.Equal("Cool", forecast.Summary);
     }
-    
+
     [Fact]
     public async Task The_user_can_get_a_weather_forecast2()
     {
-        var forecast = await _systemUnderTest.GetFromJsonAsync<WeatherForecastDto>("/WeatherForecast/One");
+        var forecast = await _systemUnderTest.GetFromJsonAsync<WeatherForecastViewModel>("/WeatherForecast/One");
 
         forecast!.Date.Should().Be("2000-01-01T00:00:00", forecast.Date);
         forecast.TemperatureC.Should().Be(10);
@@ -37,13 +37,40 @@ public class UnitTest2 : IClassFixture<WebApplicationFactory<Program>>
     }
 }
 
-public class WeatherForecastDto
+public class WeatherForecastViewModel
 {
-    public string Date { get; init; }
+    public WeatherForecastViewModel()
+    {
+        
+    }
     
+    public WeatherForecastViewModel(User[] users)
+    {
+        Users = Array.ConvertAll(users, user => new UserViewObject(user));
+    }
+
+    public string Date { get; init; }
+
     public int TemperatureC { get; init; }
 
     public int TemperatureF { get; init; }
 
-    public string? Summary { get; init;  }
+    public string? Summary { get; init; }
+
+    public UserViewObject[] Users { get; init; }
+}
+
+public class User
+{
+    public string Name { get; set; }
+}
+
+public class UserViewObject
+{
+    public string Name { get; init; }
+
+    public UserViewObject(User user)
+    {
+        Name = user.Name;
+    }
 }
